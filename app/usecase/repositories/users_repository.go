aa399e2d15cdb74366bcfecc9d3f26a08ec7ce15
Satoshi Ota <>
2020-04-17 12:11:09 +0900

    Move Repository

--- app/usecase/repositories/users_repository.go
+++ app/usecase/repositories/users_repository.go
@@ -2,8 +2,8 @@ package users
 
 import "github.com/dev-sota/gin-api/app/domain"
 
-// UserRepository interface
-type UserRepository interface {
+// Repository interface
+type Repository interface {
 	Store(domain.User) (int, error)
 	FindByID(id int) ([]domain.User, error)
 	FindAll() ([]domain.User, error)
